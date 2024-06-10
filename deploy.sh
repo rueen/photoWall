c8dc0c7887c2d0b927b591546f91cbdd35b3cd09
rueen <rueenli@gmail.com>
2024-06-11 00:25:27 +0800

    工作流

--- deploy.sh
+++ deploy.sh
@@ -9,26 +9,25 @@ set -e
 # 构建
 npm install
 npm run build
-# npm run preview
 
-# # 进入构建文件夹
-# cd dist
+# 进入构建文件夹
+cd dist
 
-# # 放置 .nojekyll 以绕过 Jekyll 的处理。
-# echo > .nojekyll
+# 放置 .nojekyll 以绕过 Jekyll 的处理。
+echo > .nojekyll
 
-# # 如果你要部署到自定义域名
-# # echo 'www.example.com' > CNAME
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
 
-# git init
-# git checkout -B main
-# git add -A
-# git commit -m 'deploy'
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
 
-# # 如果你要部署在 https://<USERNAME>.github.io
-# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:rueen/photoWall.git main:gh-pages
 
-# cd -
+cd -
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: diaochan
+ * @Date: 2024-06-08 13:01:14
+ * @LastEditors: rueen
+ * @LastEditTime: 2024-06-11 00:24:07
+ * @Description: 
+ */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/photoWall/'
+})
