f59750098342fdfbd62ab6095f9614a1a1f33d12
rueen <rueenli@gmail.com>
2024-06-11 10:19:13 +0800

    base路径

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2024-06-11 09:12:30
  * @LastEditors: rueen
- * @LastEditTime: 2024-06-11 10:10:05
+ * @LastEditTime: 2024-06-11 10:18:42
  * @Description: 
  */
 import ghpages from 'gh-pages';
@@ -10,6 +10,5 @@ import ghpages from 'gh-pages';
 ghpages.publish('dist', {
   repo: 'https://github.com/rueen/photoWall.git',
   // src: '/photoWall/dist',
-  dotfiles: true,
-  // nojekyll: true
+  nojekyll: true
 }, function(err) {});
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: diaochan
+ * @Date: 2024-06-11 10:18:46
+ * @LastEditors: rueen
+ * @LastEditTime: 2024-06-11 10:19:01
+ * @Description: 
+ */
+export default {
+  base: '/photoWall/',
+};
