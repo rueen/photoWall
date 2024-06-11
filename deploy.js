ed240b4ecc396f8ae7d77ab433c3e9bb6f83fd6d
rueen <rueenli@gmail.com>
2024-06-11 16:46:23 +0800

    deploy

--- deploy.js
+++ deploy.js
@@ -2,13 +2,12 @@
  * @Author: diaochan
  * @Date: 2024-06-11 09:12:30
  * @LastEditors: rueen
- * @LastEditTime: 2024-06-11 10:18:42
+ * @LastEditTime: 2024-06-11 16:42:44
  * @Description: 
  */
 import ghpages from 'gh-pages';
 
 ghpages.publish('dist', {
   repo: 'https://github.com/rueen/photoWall.git',
-  // src: '/photoWall/dist',
   nojekyll: true
 }, function(err) {});
